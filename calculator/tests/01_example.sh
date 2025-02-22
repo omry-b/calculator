fe7585848b27c9109069214fa7ff8bb3b7e665a2
omry-b <omry.bejerano@gmail.com>
2025-02-22 14:27:10 -0800

    ci testing

--- calculator/tests/01_example.sh
+++ calculator/tests/01_example.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/env bash
-
 # (The absolute path to the program is provided as the first and only argument.)
 CALCULATOR=$1
 
-echo "We've set up a GitHub Actions Workflow that will run all"
-echo "of the shell scripts in this directory as a series of tests."
-echo
-echo "To fail any test, you should use the exit 1 command;"
-echo "To end a test early as a success, you should use the exit 0 command."
-
-echo "Invoke your program with the \$CALCULATOR variable; an example is below"
-
-
 # Test 01: Ensure the program runs without error with a simple, valid invocation.
-if ! $CALCULATOR 1 + 1; then  # If the return code of $PROGRAM is non-zero (i.e. error)...
+if ! $CALCULATOR 1 + 1; then
   echo 'ERROR! A valid run of the calculator (1 + 1) failed!'
   exit 1
 fi
 
 # Test 02: Ensure simple case has correct output...
-if [[ $($CALCULATOR -q 1 + 1) != "2" ]]; then
+if [[ $($CALCULATOR 1 + 1) != "2" ]]; then
   echo 'ERROR! A valid run of the calculator (1 + 1) failed to produce 2 as an output!'
   exit 1
 fi
